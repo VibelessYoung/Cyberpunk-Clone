5c5dfecc933911e1e45414a604d1a50e27ea6319
Amir <vibelessyoung@gmail.com>
2026-09-11 13:07:24 +0330

    fix hero

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,22 +1,49 @@
 import Navbar from "./Navbar";
 import CyberButton from "./CyberButton";
 
-const platforms=["◉ XBOX SERIES X|S","PS5","PC","SWITCH 2","MAC"];
+const platforms = ["◉ XBOX SERIES X|S", "PS5", "PC", "SWITCH 2", "MAC"];
 
-export default function Hero(){
- return <section id="top" className="hero">
-   <img className="hero-art" src="/assets/hero-art.jpg" alt="Cyberpunk 2077 characters"/>
-   <div className="hero-yellow"/><div className="hero-noise"/>
-   <Navbar/>
-   <div className="hero-content">
-    <div className="reviews">
-      <div className="review">“BETTER THAN EVER”<small className="block font-normal">PUSH SQUARE</small></div>
-      <div className="review">“A STELLAR RPG”<small className="block font-normal">GAMESPOT</small></div>
-      <div className="review">“HARD REBOOT”<small className="block font-normal">EUROGAMER</small></div>
-    </div>
-    <h1 className="hero-title cyber-display">Get the ultimate<br/>Cyberpunk 2077<br/>experience</h1>
-    <div className="hero-actions"><CyberButton href="#buy">Buy now</CyberButton><CyberButton href="#trailer">Watch trailer</CyberButton></div>
-    <div className="platforms">{platforms.map(p=><span key={p}>{p}</span>)}</div>
-   </div>
- </section>
+export default function Hero() {
+  return (
+    <section id="top" className="hero">
+      <img
+        className="hero-art"
+        src="/assets/hero-art.jpg"
+        alt="Cyberpunk 2077 characters"
+      />
+      <div className="hero-yellow" />
+      <div className="hero-noise" />
+      <Navbar />
+      <div className="hero-content">
+        <div className="reviews">
+          <div className="review">
+            “BETTER THAN EVER”
+            <small className="block font-normal">PUSH SQUARE</small>
+          </div>
+          <div className="review">
+            “A STELLAR RPG”<small className="block font-normal">GAMESPOT</small>
+          </div>
+          <div className="review">
+            “HARD REBOOT”<small className="block font-normal">EUROGAMER</small>
+          </div>
+        </div>
+        <h1 className="hero-title cyber-display">
+          Get the ultimate
+          <br />
+          Cyberpunk 2077
+          <br />
+          experience
+        </h1>
+        <div className="hero-actions">
+          <CyberButton href="#buy">Buy now</CyberButton>
+          <CyberButton href="#trailer">Watch trailer</CyberButton>
+        </div>
+        <div className="platforms">
+          {platforms.map((p) => (
+            <span key={p}>{p}</span>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
